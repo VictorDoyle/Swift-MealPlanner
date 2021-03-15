59749db5ce53ed1eae78f2813f6636aafaeaa386
victordoyle <victor-doyle@outlook.com>
2021-03-15 17:32:39 -0400

    fixed stack view issue

--- MealPlanner/ViewController.swift
+++ MealPlanner/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+// MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 }
 
